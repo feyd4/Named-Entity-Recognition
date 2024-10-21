8d4a7f4bc7085bcda99a243ddacd20826d851079
Lata <latapangtey98@gmail.com>
2024-10-21 11:27:31 +0530

    Initial commit

---
+++ src/test/java/com/lata/nerapplication/NerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lata.nerapplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
